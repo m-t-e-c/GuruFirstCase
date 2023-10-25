9fa7f44e3e0eb122e3e7557f73636d463fb9cdb2
Emir Çelik <44502728+m-t-e-c@users.noreply.github.com>
2023-10-25 20:11:00 +0300

    feat: BasePresenter created.

---
+++ Assets/_Game/Scripts/Presenters/BasePresenter.cs
@@ -0,0 +1,20 @@
+﻿using GuruFirstCase.Interfaces;
+using UnityEngine;
+
+namespace GuruFirstCase.Presenters
+{
+    public abstract class BasePresenter<T> : MonoBehaviour
+    {
+        IViewManager _viewManager;
+
+        protected virtual void Start()
+        {
+            _viewManager = Locator.Instance.Resolve<IViewManager>();
+        }
+
+        protected void Close()
+        {
+            _viewManager?.DestroyView<T>();
+        }
+    }
+}
