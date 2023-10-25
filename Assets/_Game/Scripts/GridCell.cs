a4a9432f23b6994e3dc3f0441bf6852cf927004d
Emir Çelik <44502728+m-t-e-c@users.noreply.github.com>
2023-10-25 18:38:33 +0300

    refactor: grid chain algorithm refactored.

--- Assets/_Game/Scripts/GridCell.cs
+++ Assets/_Game/Scripts/GridCell.cs
@@ -1,9 +1,42 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace GuruFirstCase
 {
     public class GridCell : MonoBehaviour
     {
-        public bool IsFilled { get; set; }
+        [SerializeField] GameObject fillSprite;
+        public bool IsFilled { get; private set; }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        Action<GridCell> _onClicked;
+
+        void OnMouseDown()
+        {
+            if (IsFilled)
+            {
+                return;
+            }
+
+            fillSprite.SetActive(true);
+            IsFilled = true;
+            _onClicked?.Invoke(this);
+        }
+
+        public void Init(int x, int y, Action<GridCell> onClicked)
+        {
+            X = x;
+            Y = y;
+            _onClicked = onClicked;
+        }
+
+
+        public void ResetCell()
+        {
+            IsFilled = false;
+            fillSprite.SetActive(false);
+        }
     }
 }
--- Assets/_Game/Scripts/Managers/GridManager.cs
+++ Assets/_Game/Scripts/Managers/GridManager.cs
@@ -1,33 +1,43 @@
-﻿using System.Collections.Generic;
-using GuruFirstCase.Interfaces;
+﻿using System;
 using UnityEngine;
+using System.Collections.Generic;
+using GuruFirstCase.Interfaces;
+using Object = UnityEngine.Object;
 
 namespace GuruFirstCase
 {
     public class GridManager : IGridManager
     {
-        readonly List<GameObject> gridCells = new();
+        private GridCell[,] _gridCells;
+        private int _gridSize;
+        private float _cellSize;
 
         public void CreateGrid(GameObject cellPrefab, int gridSize, float cellSize)
         {
             ResetGrid();
 
-            GameObject gridParent = new GameObject($"Grid{gridSize}x{gridSize}");
-            for (int i = 0; i < gridSize; i++)
+            _gridSize = gridSize;
+            _cellSize = cellSize;
+
+            _gridCells = new GridCell[_gridSize, _gridSize];
+            GameObject gridParent = new GameObject("Grid" + _gridSize + "x" + _gridSize);
+            for (int y = 0; y < gridSize; y++)
             {
-                for (int j = 0; j < gridSize; j++)
+                for (int x = 0; x < gridSize; x++)
                 {
-                    Vector3 cellPosition = new Vector3(i * cellSize, j * cellSize, 0);
-                    GameObject cell = Object.Instantiate(cellPrefab, cellPosition, Quaternion.identity);
+                    Vector3 cellPosition = new Vector3(x * _cellSize, y * _cellSize, 0);
+                    GameObject cellObj = Object.Instantiate(cellPrefab, cellPosition, Quaternion.identity);
+                    GridCell cell = cellObj.GetComponent<GridCell>();
+                    cell.Init(x, y, CheckFilledCellChain);
                     cell.transform.SetParent(gridParent.transform);
-                    gridCells.Add(cell);
+                    _gridCells[x, y] = cell;
                 }
             }
 
-            FitAndCenterCamera(gridSize,cellSize);
+            FitAndCenterCamera(_gridSize, _cellSize);
         }
 
-        void FitAndCenterCamera(int gridSize, float cellSize)
+        private void FitAndCenterCamera(int gridSize, float cellSize)
         {
             var camera = Camera.main;
             float cameraPos = (gridSize * cellSize - 1) / 2;
@@ -37,11 +47,68 @@ namespace GuruFirstCase
 
         public void ResetGrid()
         {
-            foreach (GameObject cell in gridCells)
+            if (_gridCells != null)
             {
-                Object.Destroy(cell);
+                foreach (GridCell cell in _gridCells)
+                {
+                    if (cell != null)
+                    {
+                        Object.Destroy(cell.gameObject);
+                    }
+                }
             }
-            gridCells.Clear();
+
+            _gridSize = 0;
+            _cellSize = 0.0f;
+            _gridCells = null;
+        }
+
+        private void CheckFilledCellChain(GridCell gridCell)
+        {
+            List<GridCell> chainedCells = new List<GridCell>();
+
+            FindConnectedCells(gridCell, chainedCells);
+
+            if (chainedCells.Count >= 3)
+            {
+                foreach (GridCell cell in chainedCells)
+                {
+                    cell.ResetCell();
+                }
+
+                chainedCells.Clear();
+            }
+        }
+
+        private void FindConnectedCells(GridCell currentCell, List<GridCell> connectedCells)
+        {
+            if (connectedCells.Contains(currentCell) || !currentCell.IsFilled)
+            {
+                return;
+            }
+
+            connectedCells.Add(currentCell);
+
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (x != 0 && y != 0)
+                        continue;
+
+                    int newX = currentCell.X + x;
+                    int newY = currentCell.Y + y;
+
+                    if (newX >= 0 && newX < _gridSize && newY >= 0 && newY < _gridSize)
+                    {
+                        GridCell neighborCell = _gridCells[newX, newY];
+                        if (neighborCell.IsFilled)
+                        {
+                            FindConnectedCells(neighborCell, connectedCells);
+                        }
+                    }
+                }
+            }
         }
     }
 }
