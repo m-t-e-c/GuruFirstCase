d419e11fafc72b9fde75a5be6d7755edf857809e
Emir Çelik <44502728+m-t-e-c@users.noreply.github.com>
2023-10-25 14:25:12 +0300

    fix: grid dimension changed and grid centering fixed.

--- Assets/_Game/Scripts/Managers/GridManager.cs
+++ Assets/_Game/Scripts/Managers/GridManager.cs
@@ -6,26 +6,42 @@ namespace GuruFirstCase
 {
     public class GridManager : IGridManager
     {
-        List<GameObject> gridCells = new();
+        readonly List<GameObject> gridCells = new();
 
         public void CreateGrid(GameObject cellPrefab, int gridSize, float cellSize)
         {
+            ResetGrid();
+
+            GameObject gridParent = new GameObject($"Grid{gridSize}x{gridSize}");
             for (int i = 0; i < gridSize; i++)
             {
                 for (int j = 0; j < gridSize; j++)
                 {
-                    Vector3 cellPosition = new Vector3(i * cellSize, 0, j * cellSize);
+                    Vector3 cellPosition = new Vector3(i * cellSize, j * cellSize, 0);
                     GameObject cell = Object.Instantiate(cellPrefab, cellPosition, Quaternion.identity);
+                    cell.transform.SetParent(gridParent.transform);
                     gridCells.Add(cell);
                 }
             }
-            
-            Camera.main.transform.position = new Vector3(gridSize * cellSize * 0.5f, gridSize * cellSize * 0.5f, -10);
-            Camera.main.orthographicSize = gridSize * cellSize * 0.6f;
+
+            FitAndCenterCamera(gridSize,cellSize);
+        }
+
+        void FitAndCenterCamera(int gridSize, float cellSize)
+        {
+            var camera = Camera.main;
+            float cameraPos = (gridSize * cellSize - 1) / 2;
+            camera.transform.position = new Vector3(cameraPos, cameraPos, -10);
+            camera.orthographicSize = gridSize * cellSize + 0.5f;
         }
 
         public void ResetGrid()
         {
+            foreach (GameObject cell in gridCells)
+            {
+                Object.Destroy(cell);
+            }
+            gridCells.Clear();
         }
     }
 }
