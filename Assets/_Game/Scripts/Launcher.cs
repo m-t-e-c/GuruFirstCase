2c186474c49c29420f080100e0bf775f262d495a
Emir Çelik <44502728+m-t-e-c@users.noreply.github.com>
2023-10-25 20:13:34 +0300

    feat: addressable added and game flow created.

---
+++ Assets/_Game/Scripts/Launcher.cs
@@ -0,0 +1,40 @@
+﻿using GuruFirstCase.Interfaces;
+using GuruFirstCase.Presenters;
+using UnityEngine;
+
+namespace GuruFirstCase
+{
+    public class Launcher : MonoBehaviour
+    {
+        Locator _locator;
+
+        IGridManager _gridManager;
+        IViewManager _viewManager;
+        
+        void Start()
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            _locator = new Locator();
+
+            RegisterManagers();
+            
+            _viewManager.LoadView(new LoadViewParams<MainMenuPresenter>()
+            {
+                viewName = "MainMenuView"
+            });
+        }
+
+        void RegisterManagers()
+        {
+            _gridManager = new GridManager();
+            _locator.Register<IGridManager>(_gridManager);
+
+            _viewManager = new ViewManager();
+            _locator.Register<IViewManager>(_viewManager);
+        }
+    }
+}
